e72197ea64fc669185d3cfd3b0928d883ff98f5e
Yug patel <141060758+yugp31@users.noreply.github.com>
2025-10-16 19:43:02 +0530

    Add twoSum method to Solution class
    Implemented twoSum method to find indices of two numbers that add up to a target.

--- Data Structure/two-sum.py
+++ Data Structure/two-sum.py
@@ -1,1 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}  # Dictionary to store number and its index
 
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
